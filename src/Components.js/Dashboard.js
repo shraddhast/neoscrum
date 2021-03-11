4ae2038f8b0fad55f0ee653e7c602699b59d7888
shraddhast <shraddha.tendulkar@neosoftmail.com>
2021-03-11 12:09:46 +0530

    firstcommit

---
+++ src/Components.js/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div>
+            Dashboard
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <img class="img-responsive2 image"  
+  src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png" />
+
+    <div class="col-justify-right ">
+    <button class="button1" type="button" >Add Feedback</button>
+    <button class="button2" type="button" > Logout</button>
+    </div>
+  
+    </div>
+    </nav> 
+        </div>
+    )
+}
+
+export default Dashboard
